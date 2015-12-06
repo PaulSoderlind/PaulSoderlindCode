69018ec5513f7af43741977554344b14edab36e8
PaulSoderlind <Paul.Soederlind@gmail.com>
2015-12-06 21:21:16 +0100

    function

---
+++ EmpiricalFinancePhD/jlFiles/SRFn.jl
@@ -0,0 +1,12 @@
+function SRFn(par)
+# SRFn    Calculates the Sharpe ratio from the mean and 2nd moment
+
+  mu   = par[1]          #E(Z)
+  mu_2 = par[2]          #E(Z^2)
+  s2   = mu_2 - mu.^2
+  
+  SR   = mu./sqrt(s2)
+  
+  return SR
+
+end
