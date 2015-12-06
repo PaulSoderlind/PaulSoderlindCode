040aa5897ea6735dded7098140057e8404dc1365
PaulSoderlind <Paul.Soederlind@gmail.com>
2015-12-06 21:19:31 +0100

    function

---
+++ EmpiricalFinancePhD/jlFiles/Gmm4MomFn.jl
@@ -0,0 +1,24 @@
+function Gmm4MomFn(par,x,W=1)
+
+  mu = par[1]
+  s2 = par[2]
+  
+  g = [(x-mu) ((x-mu).^2-s2) ((x-mu).^3) ((x-mu).^4-3*s2^2)]    #Tx4
+  
+  gbar = mean(g,1)'                         #4x1
+
+  return g,gbar
+
+end  
+#------------------------------------------------------------------------------
+
+function Gmm4MomLossFn(par,x,W=1)
+
+  (g,gbar) = Gmm4MomFn(par,x,W)
+
+  Loss = gbar'W*gbar                #to be minimized
+  Loss = Loss[1]
+
+  return Loss  
+
+end    
