7043c598138378250c247e2a75aa8d1bec905c5d
PaulSoderlind <Paul.Soederlind@gmail.com>
2015-12-10 20:59:35 +0100

    Example of how to simulate normally distributed data

--- EmpiricalFinancePhD/MultNormalEx.jl
+++ EmpiricalFinancePhD/MultNormalEx.jl
@@ -16,8 +16,7 @@
 #  Paul.Soderlind@unisg.ch   Oct 2015
 #------------------------------------------------------------------------------
 
-
-mu = [-1 10]               #means of two series
+mu = [-1 10]                 #means of two series
 V  = [1   0.5;               #covariance matrix of two series
       0.5 2  ]
 
@@ -26,8 +25,8 @@ T = 1000
 P = chol(V)
 X = repmat(mu,T,1) + randn(T,2)*P    #T x 2 matrix
 
-println("means of simulated data")
+println("\n means of simulated data")
 println(round(mean(X,1),4))
-println("covariance matrix of simulated data")
+println("\n covariance matrix of simulated data")
 println(round(cov(X),4))
 
