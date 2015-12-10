6210354174e1146863d022f2bee512a121f3e3b7
PaulSoderlind <Paul.Soederlind@gmail.com>
2015-12-10 21:01:09 +0100

    Example of Sharpe ratio with confidence band

--- EmpiricalFinancePhD/SRConf2Ex.jl
+++ EmpiricalFinancePhD/SRConf2Ex.jl
@@ -32,7 +32,7 @@ println("\n","[mu mu_2]",round([mu mu_2],4))
 g    = [(Rme .- mu) (Rme.^2 .- mu_2)]    #moment conditions
 T    = size(g,1)
 gbar = mean(g,1)
-println("\n","Sample moment conditions, gbar: ",round(gbar,4))
+println("\n","Sample moment conditions, gbar ",round(gbar,4))
 S = NWFn(g,1)                     #Var[sqrt(T)*gbar], Newey-West
 D = -1
 V = inv(D*inv(S)*D')              #Var[sqrt(T)*(mu,mu_2)]
@@ -40,7 +40,7 @@ println("Cov(params)")
 println(round(V,6))
                                   #delta method
 df = [ (mu_2/(mu_2 - mu.^2).^(3/2)) (-mu/(2*(mu_2 - mu.^2).^(3/2)))]
-println("\n","Analytical derivatives",round(df,3))
+println("\n","Analytical derivatives ",round(df,3))
 
 par0 = [mu;mu_2]
 SR0  = SRFn(par0)       #Sharpe ratio, function
@@ -53,9 +53,9 @@ for j = 1:k              #loop over columns (parameters)
   btilde[j] = btilde[j] + Delta
   Jac[1,j]  = (SRFn(btilde)- SR0)/Delta
 end
-println("\n","Numerical derivatives, just for comparison",round(Jac,3))
+println("\n","Numerical derivatives, just for comparison ",round(Jac,3))
 
 Std_SR = sqrt(df*V*df'/T)
-println("\n","SR and its Std",round([SR0 Std_SR],4))
-println("SR and 90% conf band",round([SR0 (SR0-1.65*Std_SR) (SR0+1.65*Std_SR)],4))
+println("\n","SR and its Std ",round([SR0 Std_SR],4))
+println("SR and 90% conf band ",round([SR0 (SR0-1.65*Std_SR) (SR0+1.65*Std_SR)],4))
 #----------------------------------------------------------------------------
