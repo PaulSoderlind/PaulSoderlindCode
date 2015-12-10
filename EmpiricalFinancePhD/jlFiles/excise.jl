5a368d12776e085775cd9d6558d5747ed6b08723
PaulSoderlind <Paul.Soederlind@gmail.com>
2015-12-10 20:58:00 +0100

    function

--- EmpiricalFinancePhD/jlFiles/excise.jl
+++ EmpiricalFinancePhD/jlFiles/excise.jl
@@ -1,7 +1,8 @@
 function excise(x)
 
-  vv = find(!any(isnan(x),2))       
-  z = x[vv,:]           #only keep rows with no NaNs. clumsy but works
+  #vv = find(!any(isnan(x),2))       
+  vv = !any(isnan(x),2)
+  z  = x[vv,:]           #only keep rows with no NaNs
 
   return z
 
