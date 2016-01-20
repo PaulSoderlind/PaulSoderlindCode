5642e9706d1f564d8032284c55611395ae7fc830
PaulSoderlind <Paul.Soederlind@gmail.com>
2016-01-20 16:54:56 +0100

    improved spacing

--- JuliaTutorial/JuliaLec_Finance.jl
+++ JuliaTutorial/JuliaLec_Finance.jl
@@ -7,19 +7,19 @@
 #-------------------------------------------------------------------------
 
                                   #do Pkg.add("package name") if needed
-using StatsBase, Gadfly, Roots, MathProgBase, Ipopt     
+using StatsBase, Gadfly, Roots, MathProgBase, Ipopt
 
-set_default_plot_size(20cm, 13cm)  
+set_default_plot_size(20cm, 13cm)
 
 
-                                     
+
 println("\n","------------------------- CAPM regression ----------------","\n")
 
-xx  = readdlm("JuliaLecData.csv",',',header=true)      
+xx  = readdlm("JuliaLecData.csv",',',header=true)
 x   = xx[1]
 ym  = x[:,1]                 #yearmonth
 Rme = x[:,2]                 #market excess return
-Rf  = x[:,3]                 #interest rate  
+Rf  = x[:,3]                 #interest rate
 R   = x[:,4]                 #return small growth stocks
 Re  = R - Rf                 #excess returns
 T   = size(Rme,1)
@@ -28,14 +28,14 @@ x    = [ones(T) Rme]             #regressors
 y    = Re                        #just to get standard OLS notation
 b    = x\y                       #OLS
 u    = y - x*b                   #residuals
-covb = inv(x'x)*var(u)           #cov(b)  
+covb = inv(x'x)*var(u)           #cov(b)
 stdb = sqrt(diag(covb))          #std(b)
 R2   = 1 - var(u)/var(y)
 
 println("OLS coefficients and std")
-println(round([b stdb],3))    
-println("R2: ",round(R2,3)) 
-println("no. of observations: ",T)  
+println(round([b stdb],3))
+println("R2: ",round(R2,3))
+println("no. of observations: ",T)
 
 
 println("\n","---------------------- Autocorrelations ------------------","\n")
@@ -52,12 +52,12 @@ lambda = 0.95                         #weight on old volatility
 
 Rm     = Rme + Rf                     #equity market return in %
 sigma2 = var(Rm)
-mu     = mean(Rm)                       
+mu     = mean(Rm)
 
 vol2 = fill(sigma2,T)
 for t = 2:T
   vol2[t] = lambda*vol2[t-1] + (1-lambda)*(Rm[t-1]-mu)^2    #RiskMetrics approach
-end  
+end
 
 VaR95 = -(mu - 1.64*sqrt(vol2))            #VaR at 95% level
 
@@ -79,16 +79,16 @@ println("\n","------------------ Mean-variance frontier ----------------","\n")
 Rf    = 0.04                       #some inputs, here riskfree
 mu    = [0.125; 0.105; 0.07]       #mean returns
 stdd  = [0.129; 0.08; 0.1]         #std
-Corr  = [ 1   0.33   0.45;         #correlation matrix     
+Corr  = [ 1   0.33   0.45;         #correlation matrix
           0.33  1    0.05;
           0.45  0.05  1];
-Sigma  = (stdd*stdd').*Corr        #covariance matrix 
+Sigma  = (stdd*stdd').*Corr        #covariance matrix
 N      = length(mu)                #number of assets
 
 mu_p = linspace(0,0.15,151)        #average returns to calculate min(Std(R_p)) at
 
 ettor   = ones(N)                  #ettor means ones in Swedish: this a vector of those
-Sigma_1 = inv(Sigma)               #mean-variance arithmetic                
+Sigma_1 = inv(Sigma)               #mean-variance arithmetic
 A       = (ettor'Sigma_1*mu)[1]          # [1] transforms from 1x1 array to scalar
 B       = (mu'Sigma_1*mu)[1]
 C       = (ettor'Sigma_1*ettor)[1]
@@ -101,9 +101,9 @@ for i = 1:K                                #loop over mean returns in Std x  mea
   h        = ( C*(Sigma_1*mu)  - A*(Sigma_1*ettor) )/D
   w_pi     = g + h.*mu_p[i]                        #portfolio weights at mean mu_p[i]
   Var_p[i] = (w_pi'Sigma*w_pi)[1]                  #variance of portfolio with mean mu_p[i]
-end 
+end
 
-SR_T    = sqrt((mu-Rf)'Sigma_1*(mu-Rf))          #Sharpe ratio of Tangency portfolio 
+SR_T    = sqrt((mu-Rf)'Sigma_1*(mu-Rf))          #Sharpe ratio of Tangency portfolio
 CML_std = sqrt(((mu_p-Rf)./SR_T).^2)             #std according to CLM
 
 plot1 = plot(layer(x=sqrt(Var_p),y=mu_p,Geom.line(preserve_order=true),Theme(default_color=colorant"red")),
@@ -117,7 +117,7 @@ display(plot1)                                   #show plot in browser
 println("\n","---------- Mean-variance frontier w.o. short sales--------","\n")
 
 include("MyFunctions.jl")     #load in file with some functions
-                              
+
 Var_no_ss, = MeanVarNoSSPs(mu,Sigma,mu_p)   #see MyFunctions.jl file
 
 plot1 = plot(layer(x=sqrt(Var_p),y=mu_p,Geom.line(preserve_order=true),Theme(default_color=colorant"red")),
@@ -140,7 +140,7 @@ function Stdn_cdfPs(a)
   return cdf
 end
 
-function OptionBlackSPs(S,X,T,r,sigma) 
+function OptionBlackSPs(S,X,T,r,sigma)
 #Calculates Black-Scholes european option price
   d1 = ( log(S./X) + (r+1/2*sigma.^2)*T ) ./ (sigma*sqrt(T))
   d2 = d1 - sigma*sqrt(T)
@@ -158,10 +158,10 @@ plot1 = plot(x=X,y=c,Geom.line,Theme(default_color=colorant"red"),
              Guide.title("Black-Scholes call option price"),
              Guide.xlabel("strike price"),
              Guide.ylabel("option price"))
-display(plot1)                           
+display(plot1)
 
                                 #simple (crude) way to solve for implied vol
-iv = Roots.fzero(sigma->OptionBlackSPs(10,10,0.5,0.1,sigma)-c1,[-1;1])  
+iv = Roots.fzero(sigma->OptionBlackSPs(10,10,0.5,0.1,sigma)-c1,[-1;1])
 println("Implied volatility (should be the same as above): ",round(iv,3))
 
 #  LIFFE Bunds option data, trade date April 6, 1994
@@ -180,8 +180,8 @@ N = length(X)
 
 iv = fill(NaN,N)
 for i = 1:N
-  iv[i] = Roots.fzero(sigma->OptionBlackSPs(exp(-m*r)*F,X[i],m,r,sigma)-C[i],[-1;1])  
-end  
+  iv[i] = Roots.fzero(sigma->OptionBlackSPs(exp(-m*r)*F,X[i],m,r,sigma)-C[i],[-1;1])
+end
 
 println(round([X iv],4))
 
