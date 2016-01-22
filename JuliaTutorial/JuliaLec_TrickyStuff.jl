ac137227f387139babd790120b0dc82a109cca23
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-01-22 11:48:44 +0100

    better editing, added material

--- JuliaTutorial/JuliaLec_TrickyStuff.jl
+++ JuliaTutorial/JuliaLec_TrickyStuff.jl
@@ -1,4 +1,5 @@
-#  This file highlights some tricky aspects of Julia
+#  This file highlights some tricky aspects of Julia (from the perspective of
+#  a matlab user)
 #
 #
 #
@@ -7,86 +8,76 @@
 #  Paul Söderlind (Paul.Soderlind at unisg.ch), December 2015
 #-------------------------------------------------------------------------
 
-println("\n","--------------------------------------------------------------","\n")
-println("\n","------------------------ Handle NaNs -------------------------","\n")
 
-xx     = readdlm("JuliaLecData.csv",',',header=true)     #loading into matrix
-x      = xx[1]
-x[1,4] = NaN                            #set to NaN
-z      = x[:,3:4]
 
-println("First five lines of z:")
-println(z[1:5,:])
-
-if any(isnan(z))       #check if any NaNs
-  println("\nz has some missing values. Cannot do calculations on those.")
-end
-println("If data has NaNs, then most calculations give a NaN.")
-println("Standard deviations of Tx2 matrix z: ",round(std(z,1),3))
-
-println("\n","Cut out row 1, since the NaN is there")
-println("Standard deviations of (T-1)x2 matrix z[2:end,:]: ",round(std(z[2:end,:],1),3))
-
-vv = !any(isnan(z),2)             #a more automatic approach: find rows with no NaN
-z = z[vv,:]                       #only keep rows with no NaNs. clumsy but works
-println("\n","Standard deviations of automatically trimmed matrix: ",round(std(z,1),3))
-#-------------------------------------------------------------------------
-
-println("\n","--------------------------------------------------------------","\n")
-println("\n","--- A = B means that A and B are one and the same (forever)---","\n")
+println("\n\n------- A = B means that A and B are the same forever-------","\n")
 A = [2 2]
 B = A
 C = sum(B)
-D = A/2
-println("(the arrays) A,B,C,D before: ",A,B,C,D)
+D = A + 0
+println("(the arrays) old A,B,C,D: ",A," ",B," ",C," ",D)
 A[2] = 3
-println("A,B,C,D after after changing A[2]: ",A,B,C,D)
-println("\nNotice that when A is changed, then it carries
-         over to B since A and B are one and the same")
-println("Actually, if you instead changed B, then it carries over to A")
-println("\nIn contrast, C and D are not changed when A is: they are not the same as A")
+println("new A,B,C,D after after changing element A[2]: ",A," ",B," ",C," ",D)
+println("\nNotice that when A is changed, then it carries over to B since
+A and B are one and the same. Actually, if you instead changed B, then it
+would carry over to A as well. In contrast, C and D are not changed when A is:
+they are not the same as A.")
 
-println("\n","------------- A = B + 0 means A and B are not one and the same --------","\n")
-A = [2 2]
-B = A + 0
-println("A,B before: ",A,B)
-A[2] = 3
-println("A,B after after changing A[2]: ",A,B)
-#-------------------------------------------------------------------------
 
-println("\n","--------------------------------------------------------------","\n")
-println("\n","----------------- 1x1 arrays are not scalars  ----------------")
+println("\n\n------------------- 1x1 arrays are not scalars  ------------------")
 A = [1 2]
-b = [3]                                  #A + b does not work
-println("(1)--- cannot do A + b, if A is Txn array and b is 1x1 array  ---","\n")
-println("instead, use A .+ b: ",A .+ b)  #works since b is expanded ('broadcasted')
-                                         #to have same dimension as A
-
-println("\n","(2)-- cannot do A[2] = b, if A is Txn array and b is 1x1 array --","\n")
-A = [1 2]
-println("old A and b: ",A," ",b)
 b = [3]
-A[2] = b[1]                 #A[1] = b does not work, A[2] = b[1] does
-println("instead use A[2] = b[1]: "," new A ",A)
-#-------------------------------------------------------------------------
+println("\nA and b: ",A," ",b)
+println("You cannot do A + b, if A is a Txn array and b is a 1x1 array.
+Instead, use A .+ b: ",A .+ b)
+println("This works since b is expanded ('broadcasted') to have the same dimension as A")
 
-println("\n","--------------------------------------------------------------","\n")
-println("\n","-- variables CREATED in a loop are not visible outside the loop  ----")
+A[2] = b[1]
+println("\nYou cannot do A[2] = b, if A is a Txn array and b is a 1x1 array.
+Instead use A[2] = b[1]: "," new A ",A)
+
+c = ones(2)
+d = [10;11]
+println("\nc and d: ",c," ",d)
+A[2] = (c'd)[1]
+println("You cannot do A[2] = c'd, if A is a Txn array and c and d are vectors.
+Instead use A[2] = (c'd)[1]: "," new A ",A)
+
+
+println("\n\n------------------Creating variables in loop------------------")
 
 for i = 1:5
   Tor = cos(i)
 end
-println("\nTrying to print Tor would give an error message")
+println("\nvariables CREATED in a loop are not visible outside the loop
+Trying to print Tor after the loop would give an error message (try it)")
 
-println("\n","-- variables CHANGED in a loop are visible outside the loop  ----")
-Tor = []
+println("\n","In contrast, variables CHANGED in a loop are visible outside the loop")
+Oden = []
 for i = 1:5
-  Tor = cos(i)
+  Oden = cos(i)
 end
-println(round(Tor,4))
-#-------------------------------------------------------------------------
+println("Oden ",round(Oden,4))
 
 
+println("\n\n----------------------Adding rows to an array----------------------")
+
+A =  [1 11]
+B =  [3 13]
+println("\nA and B: ",A," ",B)
+println("\nTo append B at the end of A, you have to use [A;B],
+doing A[2,:] = B does not work ")
+println([A;B])
+
+
+println("\n\n---------------------------Cell arrays------------------------")
+
+println("\nCreate Any[x1,x2,...]")
+A = Any[rand(3,2),"A nice dog",27]
+println("\nThe array A: ")
+println(A)
+println("\nAlternatively, you can use A= cell(3) and fill as A[3] = 27")
+println("\nElement 3,2 of A[1]: ",A[1][3,2])
 
 
 
