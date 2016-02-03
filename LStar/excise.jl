7f6b6b264ecd3ecbe6e3e17a286c1efa4d1b36ab
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-02-03 16:52:46 +0100

    a little editing

--- LStar/excise.jl
+++ LStar/excise.jl
@@ -1,8 +1,8 @@
 function excise(x)
 
-  #vv = find(!any(isnan(x),2))       
+  #vv = find(!any(isnan(x),2))
   vv = !any(isnan(x),2)
-  z  = x[vv,:]           #only keep rows with no NaNs. clumsy but works
+  z  = x[vv,:]           #only keep rows with no NaNs
 
   return z
 
