b4f872ca7c103450ca7ce6d120feeb710723dd82
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-03-31 11:57:03 +0200

    loading and saving csv, jld, mat, xls

--- JuliaTutorialExtra/LoadSaveDataTsT.jl
+++ JuliaTutorialExtra/LoadSaveDataTsT.jl
@@ -50,13 +50,13 @@ println("\nNewMatFile.mat has been created in the current folder")
 
 fh = matopen("NewMatFile.mat")                   #open the mat file
 println("\nVariables in mat file: ",names(fh))
-C2 = read(fh,"C")                                #read variable A
+C2 = read(fh,"C")                                #read variable C
 close(fh)                                        #close the mat file
 println("\nC from mat file is ")
 println(C2)
                                                  #alternative approach
-xx = matread("NewMatFile.mat")                   #read whole mat file into xx
-C3 = xx["C"]                                     #xx is a dictionary
+xx = matread("NewMatFile.mat")                   #read whole mat file into Dict xx
+C3 = xx["C"]
 println("\nC from mat file is (2nd way of loading) ")
 println(C3)
 
@@ -66,8 +66,8 @@ println("\n\n-------------------------------------------------------------------
 println("--------------loading csv with headers, fixing missing values--------")
 
 
-xx = readdlm("Data/loadCsvTsT_Data.csv",',',header=true)     #xx is Any[] array
-x = xx[1]
+xx = readdlm("Data/loadCsvTsT_Data.csv",',',header=true)
+x = xx[1]                                        #xx is Any[] array
 println("\nx")
 println(x)
 
@@ -84,13 +84,15 @@ println("-----------------------loading xlsfile-------------------------------")
 using ExcelReaders
 #see https://github.com/davidanthoff/ExcelReaders.jl for more examples
 #Notice: you need python's xlrd libarary for this to work.
+#For instructions on how to install Python, see eg.
+#https://sites.google.com/site/paulsoderlindecon/home/software
 
 println("\n------------Approach 1: readxl-------------------")
 
 data1 = readxl("Data/readXlsTsT_Data.xlsx","Data!B2:C11")       #reading using range
 println("\nRaw input")
 println(data1)
-x1 = convert(Array{Float64},data1)
+x1 = convert(Array{Float64},data1)            #convert from DataArray to traditional matrix
 println("\nNumeric part after conversion")
 println(x1)
 
@@ -107,10 +109,5 @@ x2[x2 .== -999.99] = NaN                     #converting to NaNs
 println("\nNumeric part after changing -999.99 to NaN")
 println(x2)
 
-T  = size(data2,1)                           #converting datetimes to dates
-dN = Array{Date}(T)
-for t = 1:T
-  dN[t] = Date(data2[t,1])
-end
-println("\nBoth dates (after conversion) and numeric part")
-println([dN x2])
+#see DaysAndDatesTst.jl for how to convert the datetimes in data2[:,1]
+#------------------------------------------------------------------------------
