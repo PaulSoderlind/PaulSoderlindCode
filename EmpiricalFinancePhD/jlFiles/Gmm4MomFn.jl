917ccb7d35a431ce10f462039e660645d654ec71
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:30:47 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/jlFiles/Gmm4MomFn.jl
+++ EmpiricalFinancePhD/jlFiles/Gmm4MomFn.jl
@@ -2,23 +2,23 @@ function Gmm4MomFn(par,x)
 
   mu = par[1]
   s2 = par[2]
-  
+
   g = [(x-mu) ((x-mu).^2-s2) ((x-mu).^3) ((x-mu).^4-3*s2^2)]    #Tx4
-  
+
   gbar = mean(g,1)'                         #4x1
 
   return g,gbar
 
-end  
+end
 #------------------------------------------------------------------------------
 
 function Gmm4MomLossFn(par,x,W=1)
 
   (g,gbar) = Gmm4MomFn(par,x)
 
-  Loss = gbar'W*gbar                #to be minimized
+  Loss = 1.0 + gbar'W*gbar                #to be minimized
   Loss = Loss[1]
 
-  return Loss  
+  return Loss
 
-end    
+end
