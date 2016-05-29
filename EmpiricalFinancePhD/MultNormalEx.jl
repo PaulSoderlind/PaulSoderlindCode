8e2a999aba752dfff95bc0190aee0c002dac4204
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:32:58 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/MultNormalEx.jl
+++ EmpiricalFinancePhD/MultNormalEx.jl
@@ -21,12 +21,13 @@ V  = [1   0.5;               #covariance matrix of two series
       0.5 2  ]
 
 T = 1000
+srand(123)
 
 P = chol(V)
 X = repmat(mu,T,1) + randn(T,2)*P    #T x 2 matrix
 
-println("\n means of simulated data")
+println("\nmeans of simulated data")
 println(round(mean(X,1),4))
-println("\n covariance matrix of simulated data")
+println("\ncovariance matrix of simulated data")
 println(round(cov(X),4))
 
