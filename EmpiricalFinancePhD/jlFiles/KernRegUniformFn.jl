af00c88ffba0f39613b73bd64f671b6ee459d5b0
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:31:21 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/jlFiles/KernRegUniformFn.jl
+++ EmpiricalFinancePhD/jlFiles/KernRegUniformFn.jl
@@ -1,12 +1,13 @@
 function KernRegUniformFn(y,x,xGrid,h)
 
   Ngrid = length(xGrid)                 #number of grid points
-  
-  bHat  = fill(NaN,(Ngrid,1))            #y(t) = b[x(t)] + e(t)
-  for i = 1:Ngrid                        #loop over elements in xGrid
+
+  bHat = fill(NaN,(Ngrid,1))            #y(t) = b[x(t)] + e(t)
+
+  for i = 1:Ngrid                       #loop over elements in xGrid
     zi = (x - xGrid[i])/h
     w  = (abs(zi) .< 0.5) + 0.0         # + 0.0 transforms 'True' into 1
-    w  = w/h                            #uniform kernel over xGrid +/- h/2 
+    w  = w/h                            #uniform kernel over xGrid +/- h/2
     if sum(w)  > 0.0                    #avoids dividing by 0
       bHat[i,:] = sum(w.*y)/sum(w)      #sum over observations (data)
     end
@@ -14,4 +15,4 @@ function KernRegUniformFn(y,x,xGrid,h)
 
   return bHat
 
-end  
+end
