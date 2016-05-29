06252b4bd65ea15f07e907931d278aab77f4c836
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 16:11:34 +0200

    support both Julia 0.4.5 and 0.5.0-

--- LStar/excise.jl
+++ LStar/excise.jl
@@ -1,9 +1,57 @@
 function excise(x)
 
-  #vv = find(!any(isnan(x),2))
-  vv = !any(isnan(x),2)
-  z  = x[vv,:]           #only keep rows with no NaNs
+  vv = vec(any(isnan(x),2))
 
-  return z
+  if any(vv)              #only keep rows with no NaNs
+    x = x[!vv,:]
+  end
+
+  return x
 
 end
+#------------------------------------------------------------------------------
+
+function excise2mPs(x,y)
+
+  vv = vec(any(isnan([x y]),2))
+
+  if any(vv)              #only keep rows with no NaNs
+    x = x[!vv,:]
+    y = y[!vv,:]
+  end
+
+  return x,y
+
+end
+#------------------------------------------------------------------------------
+
+function excise3mPs(x,y,z)
+
+  vv = vec(any(isnan([x y z]),2))
+
+  if any(vv)              #only keep rows with no NaNs
+    x = x[!vv,:]
+    y = y[!vv,:]
+    z = z[!vv,:]
+  end
+
+  return x,y,z
+
+end
+#------------------------------------------------------------------------------
+
+function excise4mPs(x,y,z,w)
+
+  vv = vec(any(isnan([x y z w]),2))
+
+  if any(vv)              #only keep rows with no NaNs
+    x = x[!vv,:]
+    y = y[!vv,:]
+    z = z[!vv,:]
+    w = w[!vv,:]
+  end
+
+  return x,y,z,w
+
+end
+#------------------------------------------------------------------------------
