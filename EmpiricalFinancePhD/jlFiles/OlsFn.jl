adf54f909369ebd12e45a8d22dec8887edbabc0c
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:32:07 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/jlFiles/OlsFn.jl
+++ EmpiricalFinancePhD/jlFiles/OlsFn.jl
@@ -3,7 +3,7 @@ function OlsFn(y,x)
 #
 #
 #
-#  Usage:    (b,res,yhat,Covb,R2,T) = OlsFn(y,x)
+#  Usage:    (b,res,yhat,Covb,R2a,T) = OlsFn(y,x)
 #
 #  Input:    y            Tx1 or Txn matrix of the dependent variables
 #            x            Txk matrix of regressors (including deterministic ones)
@@ -12,7 +12,7 @@ function OlsFn(y,x)
 #            res          Tx1 or Txn matrix, residuals y - yhat
 #            yhat         Tx1 or Txn matrix, fitted values x*b
 #            Covb         matrix, covariance matrix of vec(b) =[beq1;beq2;...]
-#            R2           1xn vector, R2 values
+#            R2a          1xn vector, R2 values
 #            T            scalar, number of obs
 #
 #
@@ -22,16 +22,16 @@ function OlsFn(y,x)
 
   T = size(y,1)
   n = size(y,2)
-  
+
   b     = x\y
   yhat  = x*b
   res   = y - yhat
-  
+
   Covres = cov(res)*(T-1)/T
   Covb   = kron(Covres,inv(x'x))
-  R2     = 1 - var(res,1)./var(y,1)
-  
-  return b,res,yhat,Covb,R2,T
+  R2a    = 1 - var(res,1)./var(y,1)
+
+  return b,res,yhat,Covb,R2a,T
 
 end
 #------------------------------------------------------------------------------
