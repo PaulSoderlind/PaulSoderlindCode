ad6ee9c0d311d7acdc75523bbe868ffef122519f
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:31:13 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/jlFiles/KernRegNormalFn.jl
+++ EmpiricalFinancePhD/jlFiles/KernRegNormalFn.jl
@@ -1,14 +1,15 @@
 function KernRegNormalFn(y,x,xGrid,h,vv)
 
   Ngrid = length(xGrid)                 #number of grid points
-  
-  bHat  = fill(NaN,(Ngrid,1))            #y(t) = b[x(t)] + e(t)
+
+  bHat = fill(NaN,(Ngrid,1))            #y(t) = b[x(t)] + e(t)
+
   for i = 1:Ngrid                        #loop over elements in xGrid
-    zi = (x - xGrid[i])/h
-    w  = exp(-zi.^2/2)./(h*sqrt(2*pi))  #gaussian kernel, with "std" = h
+    zi        = (x - xGrid[i])/h
+    w         = exp(-zi.^2/2)./(h*sqrt(2*pi))       #gaussian kernel, with "std" = h
     bHat[i,:] = sum(w[vv].*y[vv])/sum(w[vv])        #sum over observations (data)
   end
 
   return bHat
 
-end  
+end
