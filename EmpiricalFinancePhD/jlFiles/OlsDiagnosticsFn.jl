868fd6694e3e241d02f86f122e8ee9c9311aace3
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:31:56 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/jlFiles/OlsDiagnosticsFn.jl
+++ EmpiricalFinancePhD/jlFiles/OlsDiagnosticsFn.jl
@@ -20,25 +20,25 @@ function OlsDiagnosticsFn(y,x,u,m=1)
 #
 #
 #
-#  Paul.Soderlind@unisg.ch  
+#  Paul.Soderlind@unisg.ch
 #------------------------------------------------------------------------------
 
   ux = excise([y u x])
   y  = ux[:,1]
   u  = ux[:,2]
   x  = ux[:,3:end]
-  
+
   T = size(x,1)
   k = size(x,2)
-  
+
   Stdu = std(u)
-  
+
   rho = autocor(u,1:m)
 
   AutoCorrStat = sqrt(T)*rho
   pval         = 2*(1 - cdf(Normal(0,1),abs(AutoCorrStat)))
   AutoCorr     = [AutoCorrStat pval]
-  
+
   BPStat       = T*sum(rho.^2,1)
   pval         = 1 - cdf(Chisq(m),BPStat)
   BoxPierce    = [BPStat pval m]
@@ -47,21 +47,21 @@ function OlsDiagnosticsFn(y,x,u,m=1)
   dwStat = mean(excise(udiff).^2,1)/Stdu^2
   pval   = NaN
   DW     = [dwStat pval]
-  
-  psi = Array(Float64,T,0)                    #matrix of cross products of x
+
+  psi = Array{Float64}(T,0)                #matrix of cross products of x
   for i = 1:k
     for j = i:k
       psi = [psi (x[:,i].*x[:,j])]         #All cross products, incl own
     end
   end
-  
-  (b,res,yhat,Covb,R2,) = OlsFn(u.^2,psi)   #White's test for heteroskedasticity
-  WhiteStat = T*R2/(1-R2)
+
+  (b,res,yhat,Covb,R2a,) = OlsFn(u.^2,psi)   #White's test for heteroskedasticity
+  WhiteStat = T*R2a/(1-R2a)
   pval      = 1 - cdf(Chisq(size(psi,2)-1),WhiteStat)
   White     = [WhiteStat pval (size(psi,2)-1)]
-  
-  (b,res,yhat,Covb,R2,) = OlsFn(y,x)           #test of regression
-  RegrStat = T*R2/(1-R2)
+
+  (b,res,yhat,Covb,R2a,) = OlsFn(y,x)           #test of regression
+  RegrStat = T*R2a/(1-R2a)
   pval     = 1 - cdf(Chisq(size(x,2)-1),RegrStat)
   Regr     = [RegrStat pval (size(x,2)-1)]
 
