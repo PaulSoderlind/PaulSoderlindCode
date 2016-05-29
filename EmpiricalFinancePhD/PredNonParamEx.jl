4782d2294176568b2695686eba319479cdc76c9a
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-05-29 15:33:34 +0200

    support both Julia 0.4.5 and 0.5.0-

--- EmpiricalFinancePhD/PredNonParamEx.jl
+++ EmpiricalFinancePhD/PredNonParamEx.jl
@@ -16,6 +16,9 @@
 #  Paul.Soderlind@unisg.ch   22 May 2013
 #------------------------------------------------------------------------------
 
+using PyPlot
+
+
 include("jlFiles/excise.jl")
 include("jlFiles/lagnPs.jl")
 include("jlFiles/KernRegNormalFn.jl")
@@ -26,7 +29,7 @@ include("jlFiles/OlsFn.jl")
 CrossValCalcIt = 1       #1: do cross validation calculations
 #------------------------------------------------------------------------------
 
-xx  = readdlm("Data/FFdSizePs.csv",',',header=true)      
+xx  = readdlm("Data/FFdSizePs.csv",',',header=true)
 R   = xx[1]
 ymd = R[:,1]             #[YearMonthDay]
 R   = R[:,11]            #returns for the size portfolio we want to study
@@ -44,7 +47,6 @@ bHat  = KernRegNormalFn(y,x,xGrid,h,1:T)
 xGridU = collect(-9.5:0.25:9.5)
 bHatU  = KernRegUniformFn(y,x,xGridU,1)
 
-using PyPlot
 figure()
   ha = plot(xGrid,bHat,xGridU,bHatU)
   xlim(minimum(xGrid),maximum(xGrid))
@@ -60,20 +62,20 @@ figure()
 sigma = std(res)
 gamm  = b[1]
 xSort = sort(x)
-x_10  = xSort[round(Int64,floor(T*0.1))]          #crude 10th and 90th percentiles
-x_90  = xSort[round(Int64,floor(T*0.9))]
+x_10  = xSort[round(Int,floor(T*0.1))]          #crude 10th and 90th percentiles
+x_90  = xSort[round(Int,floor(T*0.9))]
 
 h_crude0 = 0.6*sigma^(2/5)*abs(gamm)^(-2/5)*(x_90-x_10)^(1/5)*T^(-1/5)
 h_crude  = 0.67
-println("initial choice of h: two versions")
-println("\n",round([h_crude0 h_crude],3))
+println("\ninitial choice of h: two versions")
+println(round([h_crude0 h_crude],3))
 #------------------------------------------------------------------------------
 
 if CrossValCalcIt == 1
 
   println("Cross-validation calculations take some time")
 
-  hM = h_crude*[0.75 1 1.5 2 3 4 5 10]'
+  hM = h_crude*[0.25 0.5 0.75 1 1.5 2 3 4 5 10]'
 
   Nh   = length(hM)
   EPEM = fill(NaN,(T,Nh))
@@ -83,12 +85,11 @@ if CrossValCalcIt == 1
       b_t       = KernRegNormalFn(y,x,x[t],hM[j],v_No_t)   #fitted b[x(t)]
       tst       = (y[t] - b_t)^2
       EPEM[t,j] = tst[1]
-    end       
+    end
   end
   EPE = mean(EPEM,1)'
   println("h and EPE")
   println(round([hM EPE],4))
-  
 
   figure()
     ha = plot(hM,EPE/minimum(EPE))
