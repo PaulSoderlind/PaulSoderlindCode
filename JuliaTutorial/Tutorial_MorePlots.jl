22e6f279fb5e5ec6cf6a086fb78a61dae247f8ab
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-09-12 16:10:07 +0200

    plots with better font site

--- JuliaTutorial/Tutorial_MorePlots.jl
+++ JuliaTutorial/Tutorial_MorePlots.jl
@@ -30,28 +30,31 @@ end
 
 t = collect(-3:6/99:6)           #A FIRST PLOT
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(12,8.5))
   plot(b,loss1,linestyle="--",color="b",linewidth=1.0)
   plot(b,log(loss1),linestyle="-",color="r",linewidth=3.0)
-  title("a title")
+  title("a title",fontsize=20)
   xlim(-3,3)               # set limits of the x-axis
   ylim(-1,20)              # set limits of the y-axis
-  xlabel("b")
+  xlabel("b (with large font)",fontsize=30)
   ylabel("loss values")
   text(-2.5,0.9,"some text")
   legend(["Loss1";"log Loss1"])
   savefig("MorePlots1.pdf")      #save pdf file of the plot
 #------------------------------------------------------------------------------
 
-
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(12,8.5))        #BAR, STEP, SURFACE
   bar(b,loss1,facecolor="red",edgecolor="white",align="center",width=0.3)
   title("Bar chart")
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(12,8.5))
   step(b,loss1,linewidth=1.5)
   title("Stairs plot")
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(12,8.5))
   surf(c,b,loss2,rstride=1,cstride=1)    #rstride and cstride improves the look
   xlim(1,7)
@@ -65,12 +68,14 @@ x = randn(N,1)                         #SCATTER, HISTOGRAM
 y = rand(N,1)
 areas = rand(51)*100                   #size of the scatter points
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(10,10))
   scatter(x,y,s=areas,alpha=0.5)
   title("Scatter plot")
   xlabel("x")
   ylabel("y")
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(10,10))
   PyPlot.plt[:hist](x,21)            #long name to avoid naming conflict
   grid("on")
@@ -85,11 +90,13 @@ for i = 1:length(dN)
   dN[i] = FirstDate + Dates.Day(i-1)       #add a day
 end
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(35,35/1.41))          #basic time series plot
   plot_date(dN,cumsum(z),"k-")
   title("Cumulative z",fontsize=18)
 
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(35,35/1.41))          #control tick marks
 ax1 = axes()
   plot_date(dN,cumsum(z),"k-")
@@ -108,7 +115,7 @@ using LaTeXStrings                 #add some LaTeX to the figure
 
 t = collect(-3:6/99:6)
 
-PyPlot.matplotlib[:rc]("font", family="serif",size=10)  #font similar to LaTeX
+PyPlot.matplotlib[:rc]("font", family="serif",size=12)  #font similar to LaTeX
 figure(figsize=(12,8.5))
   plot(b,loss1)
   title(L"a title, $2 b^2 + 0.5$")
@@ -118,6 +125,7 @@ figure(figsize=(12,8.5))
   text(-2.5,5,L"$\mu_2 = \int x^2 f(x) dx$")
 #------------------------------------------------------------------------------
 
+PyPlot.matplotlib[:rc]("font",size=18)
 figure(figsize=(12,8.5))                      #subplots
 subplots_adjust(hspace = 0.4)
 subplot(221)
