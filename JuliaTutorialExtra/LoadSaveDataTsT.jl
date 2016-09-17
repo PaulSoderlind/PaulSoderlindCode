a52f9235e1e89d627bcb15d277c4e7478a795fd2
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-09-17 21:53:57 +0200

    works better with Julia 0.5

--- JuliaTutorialExtra/LoadSaveDataTsT.jl
+++ JuliaTutorialExtra/LoadSaveDataTsT.jl
@@ -2,14 +2,14 @@
 println("\n---------------------------------------------------------------------")
 println("-------------------saving and loading csv----------------------------")
 
-A = reshape(1:40,5,8)
+A = reshape(1:40,5,8) + 0               #+ 0 to break link
 writedlm("NewCsvFile.csv",A,',')
 println("NewCsvFile.csv has been created in the current folder")
 
 A2 = readdlm("NewCsvFile.csv",',')
 println("\nA and A2")
-println(A)
-println(A2)
+display(A)
+display(A2)
 
 ################################################################################
 println("\n\n---------------------------------------------------------------------")
@@ -17,21 +17,21 @@ println("-----------------------saving and loading jld file-------------------")
 
 using JLD       #see https://github.com/JuliaLang/JLD.jl for more examples
 
-B   = reshape(1:40,5,8)
+B   = reshape(1:40,5,8) + 0
 B27 = 1
 save("NewJldFile.jld","B",B,"B27",B27)
 println("NewJldFile.jld has been created in the current folder")
 
 B2 = load("NewJldFile.jld","B")
 println("\nB from jld file is")
-println(B2)
+display(B2)
                                                  #alternative approach
 xx = load("NewJldFile.jld")                      #load all variables into dictionary
 println("\nAll variables in the JLD file:")
 println(keys(xx))
 B3 = xx["B"]
 println("\nB from jld file is (2nd way of loading)")
-println(B3)
+display(B3)
 
 
 ################################################################################
@@ -40,7 +40,7 @@ println("------------------saving and loading matlab mat file-------------------
 using MAT
 #see https://github.com/simonster/MAT.jl for more examples
 
-C   = reshape(1:40,5,8)
+C   = reshape(1:40,5,8) + 0
 C27 = 1
 fh = matopen("NewMatFile.mat","w")               #create a mat file
 write(fh,"C",C)
@@ -53,13 +53,12 @@ println("\nVariables in mat file: ",names(fh))
 C2 = read(fh,"C")                                #read variable C
 close(fh)                                        #close the mat file
 println("\nC from mat file is ")
-println(C2)
+display(C2)
                                                  #alternative approach
 xx = matread("NewMatFile.mat")                   #read whole mat file into Dict xx
 C3 = xx["C"]
 println("\nC from mat file is (2nd way of loading) ")
-println(C3)
-
+display(C3)
 
 ################################################################################
 println("\n\n---------------------------------------------------------------------")
@@ -69,12 +68,12 @@ println("--------------loading csv with headers, fixing missing values--------")
 xx = readdlm("Data/loadCsvTsT_Data.csv",',',header=true)
 x = xx[1]                                        #xx is Any[] array
 println("\nx")
-println(x)
+display(x)
 
 include("readdlmFixPs.jl")                       #function for fixing missing values
 y = readdlmFixPs(x)                              #fixing missing value at x[1,4]
 println("\nafter fix")
-println(y)
+display(y)
 
 
 ################################################################################
@@ -91,23 +90,23 @@ println("\n------------Approach 1: readxl-------------------")
 
 data1 = readxl("Data/readXlsTsT_Data.xlsx","Data!B2:C11")       #reading using range
 println("\nRaw input")
-println(data1)
+display(data1)
 x1 = convert(Array{Float64},data1)            #convert from DataArray to traditional matrix
 println("\nNumeric part after conversion")
-println(x1)
+display(x1)
 
 println("\n------------Approach 2: readxlsheet--------------")
 
 data2  = readxlsheet("Data/readXlsTsT_Data.xlsx","Data",skipstartrows=1)  #reading all columns
 println("\nRaw input")
-println(data2)
+display(data2)
 x2 = convert(Array{Float64},data2[:,2:end])
 println("\nNumeric part after conversion to floating point numbers")
-println(x2)
+display(x2)
 
 x2[x2 .== -999.99] = NaN                     #converting to NaNs
 println("\nNumeric part after changing -999.99 to NaN")
-println(x2)
+display(x2)
 
 #see DaysAndDatesTst.jl for how to convert the datetimes in data2[:,1]
 #------------------------------------------------------------------------------
