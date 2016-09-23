d614a9bc5627d97dbccce6a9e39678e1d1c19784
Paul Söderlind <Paul.Soederlind@gmail.com>
2016-09-23 12:56:25 +0200

    works with Julia 0.5

--- JuliaTutorialExtra/NaNTsT.jl
+++ JuliaTutorialExtra/NaNTsT.jl
@@ -1,15 +1,20 @@
+import Formatting
+include("printmat.jl")            #just function for prettier matrix printing
+
 
 z = [1 NaN;2 12;3 13]                 #a matrix with NaNs
-println("z ",z)
+println("z: ")
+printmat(z)
 
 if any(isnan(z))                      #check if any NaNs
-  println("\nz has some missing values. Cannot do calculations on those.")
+  println("z has some NaNs")
 end
-println("If data has NaNs, then most calculations give a NaN.
-Column means of Tx2 matrix z: ",mean(z,1))
 
-println("Column means of (T-1)x2 matrix z[2:end,:]: ",mean(z[2:end,:],1))
+println("\nIf data has NaNs, then most calculations give a NaN. For, instance,
+the column means of Tx2 matrix z: ")
+printmat(mean(z,1))
 
-vv = !any(isnan(z),2)             #a more automatic approach
-z = z[vv,:]                       #keep only rows with no NaNs
-println("Column means of automatically trimmed matrix: ",mean(z,1))
+vv = vec(!any(isnan(z),2))             #an automatic approach
+z2 = z[vv,:]                           #keep only rows with no NaNs
+println("z2: a new matrix where all rows with any NaNs have been pruned:")
+printmat(z2)
