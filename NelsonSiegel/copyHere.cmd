285fb592dca958043cebfeec12f99c397a56c8be
Paul Söderlind <PaulSoderlind@users.noreply.github.com>
2018-10-08 14:16:44 +0200

    reorganised the repository

--- NelsonSiegel/copyHere.cmd
+++
@@ -1,4 +0,0 @@
-
-xcopy D:\PsCode\PsJulia\PaulS\BondNSxEstPs.jl         /D /Y
-xcopy D:\PsCode\PsJulia\PaulS\BondNSxPs.jl            /D /Y
-xcopy D:\PsCode\PsJulia\PaulS\BondPricePs.jl          /D /Y
