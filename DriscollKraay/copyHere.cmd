88d8f397e7e2103f0fc7eaa9afcb2add5663daa2
Paul Söderlind <PaulSoderlind@users.noreply.github.com>
2018-10-08 13:55:18 +0200

    not needed

--- DriscollKraay/copyHere.cmd
+++
@@ -1,5 +0,0 @@
-
-xcopy D:\PsCode\PsJulia\PaulS\FindNoNaNPs.jl       /D /Y
-xcopy D:\PsCode\PsJulia\PaulS\HDirProdPs.jl        /D /Y
-xcopy D:\PsCode\PsJulia\PaulS\HszDk5dwPs.jl        /D /Y
-xcopy D:\PsCode\PsJulia\PaulS\IterationPrintPs.jl  /D /Y
